84ad7044fb2a70e8634656e67d8f63d5ccb47a82
daction <daction-z@hotmail.com>
2015-03-10 20:31:20 -0700

    lvl 1

---
+++ plusOne.cpp
@@ -0,0 +1,51 @@
+
+
+/*
+
+ 2015/Mar/10
+
+ Given a non-negative number represented as an array of digits, plus one to the number.
+
+ The digits are stored such that the most significant digit is at the head of the list.
+
+Credit to Zhang Lei
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+vector<int> plusOne(vector<int> &digits) {
+  //Start typing your C/C++ solution below
+  //Do NOT write int main() function
+
+    int carry = 1;
+    int sum = 0;
+
+    vector<int> result(digits.size(),0);
+
+    for(int i = digits.siz()-1; i>=0; i--){
+        sum = cary + digits[i];
+        cary = sum / 10;
+        result[i] = sum % 10;
+    }
+
+    if(cary > 0){
+        result.insert(result.begin(), cary);
+
+    }
+    return result;
+
+
+
+}
+
+int main(){
+
+    
+
+    return 0;
+
+
+}
